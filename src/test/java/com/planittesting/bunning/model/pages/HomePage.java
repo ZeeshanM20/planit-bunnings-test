47826a0d53b65f5c687e2cc75e3aaf9eec51a6f3
Junho <knightt0258@gmail.com>
2021-06-24 13:17:27 +1200

    First Test

---
+++ src/test/java/com/planittesting/bunning/model/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.planittesting.bunning.model.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage<HomePage>{
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+
+    }
+    
+}
